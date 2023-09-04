bbd2d492a71e98ba2f71ef1a2f1c1f879b8d3dc0
guigomes91 <guilherme.gomes91@outlook.com>
2023-09-04 07:13:08 -0300

    If statements

--- cripto-backend/start.py
+++ cripto-backend/start.py
@@ -1,1 +1,17 @@
-print('Hello')
+amigos = ['Marcos', 'Pedro', 'Adriana', 'Karina', 'Julia']
+numeros = [3, 5, 12, 2, 15, 6]
+
+amigos.sort()
+numeros.sort()
+print(amigos)
+print(numeros)
+
+acordei_antes_9 = False
+fome = True
+
+if acordei_antes_9 and fome:
+    print('Hora de fazer o café da manhã')
+elif acordei_antes_9 and not(fome):
+    print('Hora de fazer uma corrida')
+else:
+    print('Hora de fazer o almoço')
